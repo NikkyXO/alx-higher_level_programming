07c83e57e8b7f915c7673e5a69f749eb6fabe418
NikkyXO <alexisdame2017@gmail.com>
2022-05-30 16:46:18 +0100

    modified README.md

--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 str = "Holberton School"
-<<<<<<< HEAD
-print(str*3 + '\n' + str[:9])
-=======
 print(str * 3  + '\n' + str[:9] + '\n')
->>>>>>> 33c2e47d8f2299b3b5929afa0af88bae6c409a69
