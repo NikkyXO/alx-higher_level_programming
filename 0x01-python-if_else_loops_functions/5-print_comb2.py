216491eab2ab21e46c11073c0abac90fb1c235bb
NikkyXO <alexisdame2017@gmail.com>
2022-05-31 15:33:47 +0100

    prints numbers from 0 to 99

---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def print_num_in_range_100():
+	for n in range(100):
+		if n != 99:
+			print('{:02d}'.format(n), end=', ')
+		else:
+			print('{:02d}'.format(n))
+
+if __name__ == '__main__':
+	print_num_in_range_100()
+
