98065bb3e9e2e3c6e16bf68909060dbebaf7865a
NikkyXO <alexisdame2017@gmail.com>
2022-05-31 15:57:25 +0100

    prints the ASCII alphabet, in lowercase except q and e

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
 def print_alpha_ex():
-	for n in range (97, 123):
+	for n in range(97, 123):
 		if n != 101 and n != 123:
 			print('{}'.format(chr(n)), end='')
+
 
 if __name__ == '__main__':
 	print_alpha_ex()
