3fc2e4d1a291497ed8dad408cace19e5c35df3a2
Olanike Ogundiran <96661213+NikkyXO@users.noreply.github.com>
2022-05-31 18:13:42 +0100

    Update 4-print_hexa.py

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 
-def print_hexa(number):
-	hex_str = '0123456789abcdef'
-	tens = number // 16
-	ones = number % 16
 
-	if tens > 0:
-		return hex_str[tens] + hex_str[ones]
-	else:
-		return hex_str[ones]
+def get_hexa(number):
+    hexstr = '0123456789abcdef'
+    tens = number // 16
+    ones = number % 16
+    if tens > 0:
+        return hexstr[tens] + hexstr[ones]
+    else:
+        return hexstr[ones]
 
 
 if __name__ == '__main__':
-	for num in range(99):
-		print('{:d} = 0x{}'.format(num, print_hexa(num)))
+    for number in range(99):
+        print('{:d} = 0x{}'.format(number, get_hexa(number)))
