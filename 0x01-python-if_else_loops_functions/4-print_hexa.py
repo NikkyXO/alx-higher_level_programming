d949ddb787c535e14fda2da92502985d08ed5b01
NikkyXO <alexisdame2017@gmail.com>
2022-05-31 15:33:04 +0100

    prints all numbers from 0 to 98 in decimal and in hexadecimal

---
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def print_hexa(number):
+	hex_str = '0123456789abcdef'
+	tens = number // 16
+	ones = number % 16
+
+	if tens > 0:
+		return hex_str[tens] + hex_str[ones]
+	else:
+		return hex_str[ones]
+
+if __name__ == '__main__':
+	for num in range(99):
+		print('{:d} = 0x{}'.format(num, print_hexa(num)))
