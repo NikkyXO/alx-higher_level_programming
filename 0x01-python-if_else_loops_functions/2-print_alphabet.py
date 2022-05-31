f2a04f128e69733989483a034ed4986c070ecefa
Olanike Ogundiran <96661213+NikkyXO@users.noreply.github.com>
2022-05-31 19:51:47 +0100

    Update 2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,14 +1,3 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
 for i in range(97, 123):
-    print("{}".format(chr(i)), end="")
-=======
-
-def print_low_alpha():
-	for i in range(97, 123):
-		print('{:c}'.format(i), end='')
-
-
-if __name__ == '__main__':
-	print_low_alpha()
->>>>>>> 679dacaec727d19e38af7f43f4ef8d2cfa5d558e
+    print("{:c}".format(i), end='')
