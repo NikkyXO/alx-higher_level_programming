c2d1393e8e5781f2095421b161c69ac49c4a79ba
Olanike Ogundiran <96661213+NikkyXO@users.noreply.github.com>
2022-06-08 16:39:30 +0100

    Returns a set of all elements present in only one set.

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
+    """ Returns a set of all elements present in only one set."""
     if set_1 is not None and set_2 is not None:
         return set_1 ^ set_2
