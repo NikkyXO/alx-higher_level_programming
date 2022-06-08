0955f8eb359f3c566e369b9253c06cbc771dd66d
Olanike Ogundiran <96661213+NikkyXO@users.noreply.github.com>
2022-06-08 16:50:52 +0100

    updates or add key/value into a dictionary and returns a new copy

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
     """
-        Replaces or adds key/value in a dictionary
-        key argument will be always a string
-        value argument will be any type
-        If a key exists in the dictionary, the value will be replaced
-        If a key doesn’t exist in the dictionary, it will be created
-        You are not allowed to import any module
+    updates or add key/value into a dictionary and returns a new copy
     """
-    if a_dictionary is not None:
-        new_dict = a_dictionary
-        new_dict[key] = value
-        return new_dict
+    a_dictionary.update({key: value})
+    return (a_dictionary.copy())
