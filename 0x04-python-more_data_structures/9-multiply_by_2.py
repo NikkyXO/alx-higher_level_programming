d63f71feafc186ce83e2869271a89328c82ba32d
NikkyXO <alexisdame2017@gmail.com>
2022-06-20 16:29:07 +0100

    Raised Exception

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     """
-    function that returns a new dictionary with all values multiplied by 2
+    Return a new dictionary with all values multiplied by 2
     """
-    if a_dictionary is not None:
-
-        new_dict = dict(a_dictionary)
-
-        for k, v in a_dictionary.items():
-            new_dict[k] = v * 2
-        return new_dict
+    return {key: value*2 for (key, value) in a_dictionary.items()}
