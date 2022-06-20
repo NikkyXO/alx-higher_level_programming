0f625ca4113441ecb834d9ad8025c20def329916
NikkyXO <alexisdame2017@gmail.com>
2022-06-20 13:50:19 +0100

    prints an integer

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_integer(value):
+	"""
+	prints an integer
+	Returns True if value has been correctly printed
+	Otherwise False
+	"""
+	try:
+		print("{:d}".format(value))
+		return True
+	except:
+		return False
