d59c521f2ce01aa83a9087e2ed186bc5f75b165a
NikkyXO <alexisdame2017@gmail.com>
2022-06-20 13:49:19 +0100

    prints x elements of a list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+
+	"""
+	print x elements of a list, which must only be integer
+	returns the number of elements printed
+	"""
+	num = 0
+
+	for i in range(x):
+		try:
+			print("{:d}".format(my_list[i]), end="")
+			num += 1
+		except:
+			continue
+	print()
+	return num
