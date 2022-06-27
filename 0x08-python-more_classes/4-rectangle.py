bcc826ac51f4dbd425fd53e060af24aaf676c963
NikkyXO <alexisdame2017@gmail.com>
2022-06-27 15:49:04 +0100

    DEfining a class Rectangle

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -55,3 +55,57 @@ class Rectangle:
             return 0
         else:
             return (2 * (self.__width + self.__height))
+
+    def __str__(self):
+        """
+        str method to print rectangle
+
+        Return:
+            string: The string with the # rectangle
+        """
+        string = ""
+        if self.__width == 0 or self.__height == 0:
+            return string
+        else:
+            for i in range(self.__height):
+                for j in range(self.__width):
+                    string += '#'
+                if i < (self.__height - 1):
+                    string += '\n'
+            return string
+
+    def __repr__(self):
+        """
+        provides __repr__ method for object rectangle
+
+        Returns:
+            string (str): string to get
+        """
+        return "Rectangle(" + str(self.__width) + ", " + str(self.__height) +\
+            ")"
+
+
+if __name__ == '__main__':
+    my_rectangle = Rectangle(2, 4)
+    print(str(my_rectangle))
+    print("--")
+    print(my_rectangle)
+    print("--")
+    print(repr(my_rectangle))
+    print("--")
+    print(hex(id(my_rectangle)))
+    print("--")
+
+    # create new instance based on representation
+    new_rectangle = eval(repr(my_rectangle))
+    print(str(new_rectangle))
+    print("--")
+    print(new_rectangle)
+    print("--")
+    print(repr(new_rectangle))
+    print("--")
+    print(hex(id(new_rectangle)))
+    print("--")
+
+    print(new_rectangle is my_rectangle)
+    print(type(new_rectangle) is type(my_rectangle))
