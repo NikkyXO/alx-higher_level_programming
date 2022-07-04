2af8faec4a3d72c5327706aa3443b2e68ac94f8b
NikkyXO <alexisdame2017@gmail.com>
2022-07-04 18:05:08 +0100

    Updated 4-inherits_from.py

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -3,5 +3,6 @@
 
 
 def is_kind_of_class(obj, a_class):
-    """True if the object is an instance of, or if the object is an instance of a class that inherited from, a_class ; else False"""
+    """True if the object is an instance of, or if the object is an instance\
+        of a class that inherited from, a_class ; else False"""
     return (isinstance(obj, a_class))
--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,5 +4,6 @@
 
 
 def inherits_from(obj, a_class):
-    """returns True if the object is an instance of a class that inherited from  a_class ; otherwise False"""
+    """returns True if the object is an instance of a class\
+        that inherited from  a_class ; otherwise False"""
     return (issubclass(type(obj), a_class) and type(obj) != a_class)
