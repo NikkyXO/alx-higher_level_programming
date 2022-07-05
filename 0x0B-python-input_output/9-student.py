a7ec49c9b046135abfcbfb9c95a5386062f7f983
NikkyXO <alexisdame2017@gmail.com>
2022-07-05 17:21:47 +0100

    File inputs and outputs

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
-
-"""This module defines a class "Student" """
+"""Contains the clas "Student"""
 
 
 class Student:
-    """Instantiation of a student"""
+    """Representation of a student"""
 
     def __init__(self, first_name, last_name, age):
-        """Initializes the Student"""
+        """Initializes the student"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json_string(self):
-        """Returns a dictionary representation of a Student
-        instance"""
+    def to_json(self):
+        """returns a dictionary representation of a Student instance"""
         return self.__dict__
